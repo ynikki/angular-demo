f613951768b3e6499bbfa52991c366310c8cfd33
Nikki K <808nikz@gmail.com>
2016-09-17 15:30:27 -1000

    step12_service

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,7 +1,9 @@
 angular.module('myApp')
-  .controller('myController', ['$scope', 'mainCharacter', 'CharacterVersionFactory', function ($scope, mainCharacter, CharacterVersionFactory) {
-    $scope.myFirstName = 'Pooped';
-    $scope.myModel = 'Ready Player One';
-    $scope.mainCharacter = mainCharacter;
-    $scope.characterVersion = CharacterVersionFactory;
+  .controller('myController', ['$scope', 'mainCharacter', 'CharacterVersionFactory', 'BookService', 
+    function ($scope, mainCharacter, CharacterVersionFactory, BookService) {
+      $scope.myFirstName = 'Pooped';
+      $scope.myModel = 'Ready Player One';
+      $scope.mainCharacter = mainCharacter;
+      $scope.characterVersion = CharacterVersionFactory;
+      $scope.books = BookService.getBooks();
   }]);
---
+++ public/js/services.js
@@ -0,0 +1,31 @@
+angular.module('myApp')
+  .service('BookService', BookService);
+
+function BookService () {
+  this.value = "I am a Book Service!";
+  this.books =[
+    {
+      id: 1,
+      title: 'The Bell Jar',
+      author: 'Slyvia Plath'
+    },
+    {
+      id: 2,
+      title: '1984',
+      author: 'George Orwell'
+    },
+    {
+      id: 3,
+      title: 'Animal Farm',
+      author: 'George Orwell'
+    }
+  ]
+}
+
+BookService.prototype.getBooks = function () {
+  return this.books;
+}
+
+BookService.prototype.getBook = function (id) {
+  return this.books.find((book) => book.id === id);
+}
